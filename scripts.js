66e158cc40b0586ee5b2a0df13eed72d7aa250ec
Rut Bj <rub6@hi.is>
2018-11-13 17:39:05 +0000

    first commit

--- scripts.js
+++ scripts.js
@@ -1,130 +1,123 @@
-// const API_URL = '/example.json?domain=';
-const API_URL = 'http://apis.is/isnic?domain=hi.i';
-
-/**
- * Leit að lénum á Íslandi gegnum apis.is
- */
+const API_URL = 'https://apis.is/isnic?domain=';
 const program = (() => {
   let input;
   let results;
-  function init(domains) {//setja upp allt sem við þurfum þegar forritið byrjar að keyra
-
-    const form = domains.querySelector('form');
-    input = form.querySelector('input');
-    results = domains.querySelector('.results');
-
-    form.addEventListener('submit', onSubmit);
+  function el(type, text) {
+    const eli = document.createElement(type);
+    if (text) {
+      eli.appendChild(document.createTextNode(text));
+    }
+    return eli;
   }
-
-  function onSubmit(e){
-    e.preventDefault();
-
-    const plate = input.value;   
-    console.log(plate);   //það sem við skrifum kemur í console.log
-
-    fetchResults(plate);
-  // showLoading();
-  }
-
-  function showLoading(){
-    const img = el('img');
-    img.setAttribute('alt','loading gif');
-    img.setAttribute('src', 'loading.gif');
-
-    const imageDiv= el('div');
-    imageDiv.classList.add('loading');
-    imageDiv.appendChild(img);
-
-    results.appendChild(imageDiv);
-  }
-
-  function showMessage(message){
-    //setha skilaboð í reluts hlut í html
-  }
-
-  function showResults(data){
-    erase(results);
-    //let {type, number, color} = data[0];
-    let dataObj = data[0];
-
-    //console.log(type, number, color); // buið að taka uppl/gildi úr hlutnum
-
-    let dlElement = document.createElement('dl');
-
-   // let dt = el('dt','Type');
-    //document.createElement('dt');
-    //dt.appendChild(document.createTextNode('Type'));
-
-    //let dd = el('dd', type);
-    // document.createElement('dd');
-    //dd.appendChild(document.createTextNode(type));
-
-    //let div = el('div'); //ef við viljum búa til tómt div
-
-   // dlElement.appendChild(dt);
-   // dlElement.appendChild(dd); //copyaði niður
-   let dd;
-   let dt;
-
-   for(let key in dataObj){
-    dd = el('dd', key);
-    dt = el('dt', dataObj[key]); //það sem er bakvið þessu gildi
-    dlElement.appendChild(dd);
-    dlElement.appendChild(dt);
-   }
-  
-    results.appendChild(dlElement);
-
-  }
-
-  function erase(container){
-    while (container.firstChild){
+  function erase(container) {
+    while (container.firstChild) {
       container.removeChild(container.firstChild);
     }
   }
+  function showMessage(message) {
+    erase(results);
+    const thing = el('p', message);
+    results.appendChild(thing);
+  }
+  function showLoading() {
+    erase(results);
+    const img = el('img');
+    img.setAttribute('alt', 'loading gif');
+    img.setAttribute('src', 'loading.gif');
 
-  function el(type, text){
-    let el = document.createElement(type);
-    if (text){
-      el.appendChild(document.createTextNode(text));
+    const imageDiv = el('div');
+    imageDiv.classList.add('loading');
+    imageDiv.appendChild(img);
+
+    const load = el('p', 'Leita að léni ...');
+    imageDiv.appendChild(load);
+    results.appendChild(imageDiv);
+  }
+  function changeDate(dags) {
+    const dag = new Date(dags);
+    const dagur = dag.getDate();
+    const man = parseInt(dag.getMonth(), 10) + 1;
+    const ar = dag.getFullYear();
+    dags = ar + "-" + man + "-" + dagur; // eslint-disable-line
+    return dags;
+  }
+  function birta(dataObj, list, key) {
+    const dlElement = document.createElement('dl');
+    const newDataObj = dataObj;
+    if (key === 'registered') {
+      newDataObj[key] = changeDate(dataObj[key]);
     }
-    return el;
+    if (key === 'expires') {
+      newDataObj[key] = changeDate(dataObj[key]);
+    }
+    if (key === 'lastChange') {
+      newDataObj[key] = changeDate(dataObj[key]);
+    }
+    const dt = el('dt', list[key]);
+    const dd = el('dd', dataObj[key]);
+    dlElement.appendChild(dt);
+    dlElement.appendChild(dd);
+    results.appendChild(dlElement);
   }
 
+  function showResults(data) {
+    erase(results);
+    const dataObj = data[0];
+    const translate = {
+      domain: 'Lén',
+      registrantname: 'Skráningaraðili',
+      address: 'Heimilisfang',
+      country: 'Land',
+      email: 'Netfang',
+      registered: 'Skráð',
+      expires: 'Rennur út',
+      lastChange: 'Seinast breytt',
+    };
+    const fast = JSON.parse(JSON.stringify(translate, ['domain', 'registered', 'expires', 'lastChange'], 4));
+    const auka = JSON.parse(JSON.stringify(translate, ['registrantname', 'email', 'address', 'country'], 4));
 
-  function fetchResults(plate){
-    fetch(`${API_URL}${plate}`)
-    .then( (data) => {
-      if (!data.ok){
-        throw new Error('Non 200 status');
+    for (const key in fast) {
+      birta(dataObj, fast, key);
+    }
+    for (const key in auka) {
+      if (dataObj[key]) {
+        birta(dataObj, auka, key);
       }
-      return data.json();
-    } ) //taka á móti því þegar það kemur
-    .then ( (data => showResults(data.results))) //þetta keyrist ef allt gengur upp
-    .catch( (error)=> {
-     // console.error('ERROR', error);
-      showMessage('Villa kom upp');
-    });
+    }
   }
-
+  function fetchResults(web) {
+    if (web === '') showMessage('Lén verður að vera strengur');
+    else {
+      fetch(`${API_URL}${web}`)
+        .then((data) => {
+          if (!data.ok) {
+            throw new Error('Villa við að sækja gögn');
+          }
+          return data.json();
+        })
+        .then((data => showResults(data.results)))
+        .catch((error) => {
+          showMessage('Lén er ekki skráð');
+        });
+    }
+  }
+  function onSubmit(e) {
+    e.preventDefault();
+    const web = input.value;
+    showLoading();
+    fetchResults(web);
+  }
+  function init(domains) { // setja upp allt sem við þurfum þegar forritið byrjar að keyra
+    const form = domains.querySelector('form');
+    input = form.querySelector('input');
+    results = domains.querySelector('.results');
+    form.addEventListener('submit', onSubmit);
+  }
   return {
     init,
   };
 })();
-
-document.addEventListener('DOMContentLoaded', () => { //
-  const domains= document.querySelector('.domains')
-  console.log(domains);
+document.addEventListener('DOMContentLoaded', () => {
+  const domains = document.querySelector('.domains');
   program.init(domains);
-  
- // program.init(domains);
 });
-
-/*
-moduls
-
-
-
-
-
-*/
